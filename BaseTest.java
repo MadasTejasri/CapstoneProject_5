27c0d5a3b11545ea6f224518df201f96866a60dc
MadasTejasri <142039274+MadasTejasri@users.noreply.github.com>
2026-02-12 11:53:07 +0530

    Create BaseTest.java

---
+++ BaseTest.java
@@ -0,0 +1,30 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    public static WebDriver driver;
+
+    @BeforeClass
+    public void launchBrowser() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        
+    }
+
+    @AfterClass
+    public void closeBrowser() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
